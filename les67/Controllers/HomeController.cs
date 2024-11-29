b9bd59f768c662d16e1749bd2ab96132704b0e1b
Eldos Yksanov <eldosyksanov@gmail.com>
2024-11-29 15:52:49 +0600

    added project with unit testing

---
+++ les67/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IndexViewModel ivm = new IndexViewModel();
+            ViewData["Message"] = "Добрый день, это тестовый текст на главной странице";
+            ViewResult vr = View("Main", ivm);
+            vr.StatusCode = 200;
+            return vr;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("Policy");
+        }
+
+        public IActionResult TestPage(int page)
+        {
+            ViewBag.PageIncrement = ++page;
+            return View(new TestPageViewModel{Page = page});
+        }
+
+        
+    }
+}
