6c8543c0bb6441996ed5e63e82d39bf459aab8fc
Eldos Yksanov <eldosyksanov@gmail.com>
2024-12-03 16:44:56 +0600

    testing all Test to Controller

--- les67/obj/Debug/net8.0/les67.AssemblyInfo.cs
+++ les67/obj/Debug/net8.0/les67.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("les67")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d738792a4f5b7ca0914949389d0cf2ba4664a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd49ab91b4cdce0002e593c58e6df1f78f62c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("les67")]
 [assembly: System.Reflection.AssemblyTitleAttribute("les67")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
